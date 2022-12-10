6cd75a608ee4351df814d4a959db4b81e345f7af
fellipefontanez <106833552+fellipefontanez@users.noreply.github.com>
2022-12-10 18:09:36 -0300

    corrigindo URI

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -10,7 +10,7 @@ const Login = () =>{
             <img className={styles.logo} src={logo} alt=""/>
             <h2 className={styles.title}>Bem vindo ao Picture in time!</h2>
             <p className={styles.paragrafo}>Para continuar você precisa fazer login com sua conta Unsplash</p>
-            <a className={styles.login_botao} href="https://unsplash.com/oauth/authorize?client_id=l2yXgd4UaAleOP2j7R9VneHn_66R3_444bjxNkmVRwY&redirect_uri=react-picture-in-time-l4zskhi3v-fellipefontanez.vercel.app/feed&response_type=code&scope=public+read_user+read_photos+write_likes">
+            <a className={styles.login_botao} href="https://unsplash.com/oauth/authorize?client_id=l2yXgd4UaAleOP2j7R9VneHn_66R3_444bjxNkmVRwY&redirect_uri=https://react-picture-in-time-l4zskhi3v-fellipefontanez.vercel.app/feed&response_type=code&scope=public+read_user+read_photos+write_likes">
             <Icon icon="ri:unsplash-fill" /> Entrar
             </a>
         </main>
