767864e052e0a5accdd4b3461df8ade62f2d1b43
fellipefontanez <106833552+fellipefontanez@users.noreply.github.com>
2022-12-10 02:39:31 -0300

    adicionados: imagem de usuario, nome, estilização, testes

--- src/pages/home/App.tsx
+++ src/pages/home/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import {useEffect} from 'react';
 import './index.css';
 import axios from 'axios';
 import data from '../../objeto.json'
 
 
 function App() {
-  console.log(data[0]);
+
+  const dados = data;
 
   return (
     <div className="main-container">
-      {data.map(item => {
-      return(
-      <div className="container-componente-foto" key={item.id}>
-        <div className="foto"><img src={item.urls.small} alt="" /></div>
-        <div className="container-legenda">
-          <div className="legenda"><span className="legenda-foto">{item.alt_description}</span></div>
-          <div className="likes"><span className="like-count">{item.likes} Likes</span></div>
-        </div>
-      </div>
+      
+      {dados.map(item => {
+        return(
+        <div className="container-componente-foto" key={item.id} >
+          <div className="user-container">
+            <div className="foto-user"><img src={item.user.profile_image.medium} alt="user" className="foto-usuario" key={item.id}/></div>
+            <div key={item.id} className="nome-usuario"><span>{item.user.name}</span></div>
+          </div>
+          <div key={item.id} className="foto" ><img src={item.urls.small} alt="" /></div>
+          <div className="container-legenda">
+            <div className="legenda"><span className="legenda-foto">{item.alt_description}</span></div>
+            <div key={item.id} className="likes">
+              <span className="like-count">{item.likes} Likes</span>
+            </div>
+          </div>
+          </div>
       )})}
+
+      <div className="mostrar-mais"><button onClick={App}>Ver outras</button>
+      </div>
     </div>
+
   );
 }
 
